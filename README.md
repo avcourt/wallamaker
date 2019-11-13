663277182b31d6f94e5b723891288961baa4d940
avcourt <andrew.vcourt@gmail.com>
2019-11-13 12:50:30 -0600

    max resolution var

---
+++ README.md
@@ -0,0 +1,22 @@
+![contact sheet](contact-sheet.jpg)
+### hi_res/pic1.jpg}
+![hi_res/pic1.jpg](hi_res/pic1.jpg)
+***
+### hi_res/pic2.jpg}
+![hi_res/pic2.jpg](hi_res/pic2.jpg)
+***
+### hi_res/pic3.jpg}
+![hi_res/pic3.jpg](hi_res/pic3.jpg)
+***
+### hi_res/pic4.jpg}
+![hi_res/pic4.jpg](hi_res/pic4.jpg)
+***
+### hi_res/pic5.jpg}
+![hi_res/pic5.jpg](hi_res/pic5.jpg)
+***
+### hi_res/pic6.jpg}
+![hi_res/pic6.jpg](hi_res/pic6.jpg)
+***
+### hi_res/pic7.jpg}
+![hi_res/pic7.jpg](hi_res/pic7.jpg)
+***
--- wallamaker.sh
+++ wallamaker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+RES=2000 # max thumb resolution, if you are running out of memory and
+         # mogrify or montage is failing, try lowering this value.
+
 echo "Checking directory structure."
 if [ -d hi_res ] && [ -d thumbs ]; then
     echo "It looks like you have run wallamaker.sh before."
@@ -7,15 +10,15 @@ if [ -d hi_res ] && [ -d thumbs ]; then
     echo "Note: NEW photos should be placed in the directory root, not hi_res/."
     rm -v contact-sheet.jpg
     echo "Updating thumbnails with new images..."
-    echo "Generating new thumbnails with max dimension 500 pixels"
-    mogrify -format jpg -path thumbs -thumbnail 500x500 *.{gif,jpeg,jpg,png} 2>/dev/null
+    echo "Generating new thumbnails with max dimension $RES pixels"
+    mogrify -format jpg -path thumbs -thumbnail "$RES"x"$RES" *.{gif,jpeg,jpg,png} 2>/dev/null
     mv -v *.{gif,jpeg,jpg,png} hi_res 2>/dev/null
 else
     echo "Creating hi_res/ and /thumb/ directories"
     mkdir hi_res thumbs
     mv *.{gif,jpeg,jpg,png} hi_res 2>/dev/null
-    echo "Generating thumbnails with max dimension 500 pixels"
-    mogrify -format jpg -path thumbs -thumbnail 500x500 hi_res/*
+    echo "Generating thumbnails with max dimension $RES pixels"
+    mogrify -format jpg -path thumbs -thumbnail "$RES"x"$RES" hi_res/*
 fi
 
 echo "Generating contact sheet"
